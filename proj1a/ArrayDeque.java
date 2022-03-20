25504eabbb6a41d02d059555281aa2e556a1eeb4
AkaikeNotFound <1427235005@qq.com>
2022-03-21 00:44:33 +0800

    Done, again, again, again, again

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -14,132 +14,134 @@ public class ArrayDeque<T> {
   }
 
   /* public ArrayDeque(T item) { */
-    /* items = (T []) new Object[8]; */
-    /* items[0] = item; */
-    /* size = 1; */
-    /* begin = 0; */
-    /* end = 1; */
-    /* this.rateupdate(); */
+  /* items = (T []) new Object[8]; */
+  /* items[0] = item; */
+  /* size = 1; */
+  /* begin = 0; */
+  /* end = 1; */
+  /* this.rateupdate(); */
   /* } */
 
-  private void rateupdate(){
-    rate = size*1.0/items.length;
+  private void rateupdate() {
+    rate = size * 1.0 / items.length;
   }
 
-  private void copy(){
-    T[] newitems = (T[]) new Object[size*4];
+  private void copy() {
+    T[] newitems = (T[]) new Object[size * 4];
     if (begin > end) {
-      System.arraycopy(items, begin, newitems, 0, size-1 - begin);
-      System.arraycopy(items, 0, newitems, size-1-begin, begin);
+      System.arraycopy(items, begin, newitems, 0, size - begin);
+      System.arraycopy(items, 0, newitems, size - begin, begin);
     } else {
-      System.arraycopy(items, begin, newitems, 0, size-1);
+      System.arraycopy(items, begin, newitems, 0, size);
     }
     begin = 0;
-    end = size - 2;
+    end = size;
     items = newitems;
   }
 
   private void resizing() {
-    if (size > items.length) {
+    if (size == items.length) {
       this.copy();
     }
-    if (rate < 0.25 && items.length > 16){
-      this.copy(); 
+    if (rate < 0.25 && items.length > 16) {
+      this.copy();
     }
   }
 
   public void addFirst(T item) {
-    if(size == 0){
+    if (size == 0) {
       size = size + 1;
       begin = 0;
       end = begin;
       items[begin] = item;
-    }else{
-    size = size + 1;
-    this.rateupdate();
-    this.resizing();
-    if (begin == 0) {
-      begin = items.length - 1;
     } else {
-      begin = begin - 1;
+      this.rateupdate();
+      size = size + 1;
+      this.resizing();
+      if (begin == 0) {
+        begin = items.length - 1;
+      } else {
+        begin = begin - 1;
+      }
+      items[begin] = item;
     }
-    items[begin] = item;
-  }}
+  }
 
-  public void addLast(T item){
-    if(size == 0){
+  public void addLast(T item) {
+    if (size == 0) {
       end = 0;
       begin = end;
       size = size + 1;
       items[end] = item;
-    }else{
-    size = size + 1;
-    this.rateupdate();
-    this.resizing();
-    if (end == items.length - 1){
-      end = 0;
-    }else{
-      end = end + 1;
+    } else {
+      this.rateupdate();
+      size = size + 1;
+      this.resizing();
+      if (end == items.length - 1) {
+        end = 0;
+      } else {
+        end = end + 1;
+      }
+      items[end] = item;
     }
-    items[end] = item;
-  }}
+  }
 
-  public boolean isEmpty(){
-    if (size == 0){
+  public boolean isEmpty() {
+    if (size == 0) {
       return true;
-    }else{
+    } else {
       return false;
     }
   }
 
-  public int size(){
+  public int size() {
     return size;
   }
 
-  public void printDeque(){
+  public void printDeque() {
     int curr = begin;
-    for(int i=0;i<size;i++){
+    for (int i = 0; i < size; i++) {
       System.out.print(items[curr] + " ");
-      
-      if (curr == items.length - 1){
+
+      if (curr == items.length - 1) {
         curr = 0;
-      }else{
-      curr = curr + 1;
+      } else {
+        curr = curr + 1;
       }
     }
   }
 
-  public T removeFirst(){
+  public T removeFirst() {
     size = size - 1;
     this.rateupdate();
     this.resizing();
-    T temp = items[begin]; 
-    if (begin == items.length - 1){
+    T temp = items[begin];
+    if (begin == items.length - 1) {
       begin = 0;
-    }else{
+    } else {
       begin = begin + 1;
     }
     return temp;
   }
 
-  public T removeLast(){
+  public T removeLast() {
     size = size - 1;
     this.rateupdate();
     this.resizing();
     T temp = items[end];
-    if(end == 0){
+    if (end == 0) {
       end = items.length - 1;
-    }else{
+    } else {
       end = end - 1;
     }
     return temp;
   }
 
-  public T get(int index){
-    if (begin+index >= items.length){
-      return items[begin+index - items.length-1];
-    }else{
-      return items[begin+index];
+  public T get(int index) {
+    if (begin + index >= items.length) {
+      return items[begin + index - items.length - 1];
+    } else {
+      return items[begin + index];
     }
   }
 
