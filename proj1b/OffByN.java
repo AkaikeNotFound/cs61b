a4714109e8d822502dcb0d27c34bcee0fb8a17cc
AkaikeNotFound <1427235005@qq.com>
2022-03-24 00:52:52 +0800

    update method for non-alphabetical characters

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -1,10 +1,14 @@
 public class OffByN implements CharacterComparator{
     private int n;
+
     public OffByN(int N){
         n = N;
     }
 
     public boolean equalChars(char x, char y){
+        if(!Character.isLetter(x) || !Character.isLetter(y)){
+            return false;
+        }
         int diff = Character.getNumericValue(x) - Character.getNumericValue(y);
         return diff == n || diff == -n ;
     }
--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -1,6 +1,9 @@
 public class OffByOne implements CharacterComparator {
     @Override
     public boolean equalChars(char x, char y) {
+        if(!Character.isLetter(x) || !Character.isLetter(y)){
+            return false;
+        }
         int diff = Character.getNumericValue(x) - Character.getNumericValue(y);
         return diff == 1 || diff == -1 ;
     }
