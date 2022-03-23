c06230f46bc638cf697d0ccc203bee134a239e61
AkaikeNotFound <1427235005@qq.com>
2022-03-24 00:51:18 +0800

    done

--- proj1b/TestPalindrome.java
+++ proj1b/TestPalindrome.java
@@ -1,11 +1,14 @@
 import org.junit.Test;
+
+import java.sql.Array;
+
 import static org.junit.Assert.*;
 
 public class TestPalindrome {
-    /*// You must use this palindrome, and not instantiate
+    // You must  use this palindrome, and not instantiate
     // new Palindromes, or the autograder might be upset.
     static Palindrome palindrome = new Palindrome();
-
+    static CharacterComparator cc = new OffByOne();
     @Test
     public void testWordToDeque() {
         Deque d = palindrome.wordToDeque("persiflage");
@@ -14,5 +17,23 @@ public class TestPalindrome {
             actual += d.removeFirst();
         }
         assertEquals("persiflage", actual);
-    } Uncomment this class once you've created your Palindrome class. */
+    }
+    /*Uncomment this class once you've created your Palindrome class. */
+    @Test
+    public void testisPalindrome(){
+        boolean a = palindrome.isPalindrome("cat");
+        assertFalse(palindrome.isPalindrome("cat"));
+        assertFalse(palindrome.isPalindrome("nmsl"));
+        assertFalse(palindrome.isPalindrome("caonima"));
+        assertFalse(palindrome.isPalindrome("nishizhu"));
+
+        assertTrue(palindrome.isPalindrome("nusun"));
+
+        assertTrue(palindrome.isPalindrome("flake",cc));
+    }
+
+    public void main(String[] args){
+        testWordToDeque();
+        testisPalindrome();
+    }
 }
